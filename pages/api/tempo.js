99f6ab8b3c180247d896d2e7696cab61833ad237
mateusveloso <mateusveloso.developer@gmail.com>
2021-09-01 22:05:19 -0300

    Adicionando ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,18 @@
-function Tempo(request, response) {
+async function Tempo(request, response) {
     const dynamicDate = new Date();
-
+    const apiCep = process.env.API_CEP;
+    // http://cep.la/
+    const subscribersResponse = await fetch(`${apiCep}/53433240`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    const subscribersResponseJson = await subscribersResponse.json();
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        subscribersResponseJson
     })
 }
 
