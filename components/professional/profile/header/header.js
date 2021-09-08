66051cb2c42815c16ee493717bdfcffe61f1ea93
mateusveloso <mateusveloso.developer@gmail.com>
2021-09-07 23:06:22 -0300

    Adicionando responsive

--- components/professional/profile/header/header.js
+++ components/professional/profile/header/header.js
@@ -1,7 +1,7 @@
 import style from './header.module.css';
 import Image from 'next/image';
-import { Container, Row, Col } from 'react-bootstrap';
-import { faTooth, faStar, faGem } from '@fortawesome/free-solid-svg-icons';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { faTooth, faStar, faGem, faTrophy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
@@ -14,7 +14,7 @@ function Header() {
         <>
             <Container fluid className={style.container__header} >
                 <Row>
-                    <Col xs={8} style={{ "padding": "20px" }}>
+                    <Col sm={1} xl={8} style={{ "padding": "20px" }}>
                         <div className={style.container__image_profile}>
                             <Image src="/rudy.jpg" alt="me" layout='fill' />
                         </div>
@@ -34,16 +34,16 @@ function Header() {
                                 </OverlayTrigger> */}
                             </h5>
                             <h6>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralho</h6>
-                            <div style={{color: "gold"}}>
+                            <div style={{ color: "gold" }}>
                                 <FontAwesomeIcon icon={faStar} />
                                 <FontAwesomeIcon icon={faStar} />
                                 <FontAwesomeIcon icon={faStar} />
                                 <FontAwesomeIcon icon={faStar} />
-                                <FontAwesomeIcon icon={faStar} /> <strong style={{color: "#999"}}>( Excelente )</strong>
+                                <FontAwesomeIcon icon={faStar} /> <strong style={{ color: "#999" }}>( Excelente )</strong>
                             </div>
                         </div>
                     </Col>
-                    <Col style={{ "padding": "20px" }}>
+                    <Col style={{ "padding": "20px" }} sm={1} xl={4}>
                         <div className={style.container__info_contact}>
                             <h4>Informações Pessoais</h4>
                             <h6>E-mail: rudemar@gmail.com</h6>
@@ -59,11 +59,35 @@ function Header() {
 
             <Container fluid style={{ "background": "transparent", "borderBottom": "0px" }} className={style.container__header} >
                 <Row >
-                    <Col xs={4} style={{ "padding": "20px", "background": "white" }}>
-                        <Statistics />
+                    <Col sm={1} xl={4}>
+                        <div style={{ "padding": "20px", "background": "white", "maxHeight": "220px" }}>
+                            <Statistics />
+                        </div>
                     </Col>
-                    <Col xs={8} style={{ "padding": "20px" }}>
-                        Teste
+                    <Col sm={1} xl={8} style={{ "padding": "20px", "background": "white" }}>
+                        <h5 style={{ "border-bottom": "1px solid #ccc", "padding-bottom": "15px", "margin-bottom": "20px" }}><FontAwesomeIcon icon={faTrophy} style={{ "margin-right": "10px" }} />Ultimas avaliações </h5>
+                        <Card style={{ width: '13rem', float: 'left' }}>
+                            {/* <Image src="/rudy.jpg" alt="me" layout='fill' /> */}
+                            <Card.Img variant="top" src="/geniheire.png" />
+                            <Card.Body>
+                                <Card.Title>Geniheire</Card.Title>
+                                <Card.Text>
+                                    Excelente profissional
+                                </Card.Text>
+                                <Button variant="primary">Ver avaliação</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '13rem', float: 'left', marginLeft: '10px' }}>
+                            {/* <Image src="/rudy.jpg" alt="me" layout='fill' /> */}
+                            <Card.Img variant="top" src="/eduarda.png" />
+                            <Card.Body>
+                                <Card.Title>Eduarda</Card.Title>
+                                <Card.Text>
+                                    Adorei fazer serviço com ele, excelente!
+                                </Card.Text>
+                                <Button variant="primary">Ver avaliação</Button>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
